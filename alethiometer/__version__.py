dbceeef879cc0f68e9ccd9cb97b55615aa480f5d
MH.Xu <Minghao.Xu.1@warwick.ac.uk>
2023-04-28 23:21:41 +0100

    enable tenas, lrn, ntk, new-zen, bug-free zc_proxy

--- alethiometer/__version__.py
+++ alethiometer/__version__.py
@@ -1,12 +1,12 @@
 '''
 Author: ViolinSolo
 Date: 2023-04-07 16:08:57
-LastEditTime: 2023-04-28 17:08:33
+LastEditTime: 2023-04-28 23:20:39
 LastEditors: ViolinSolo
 Description: 
 FilePath: /zero-cost-proxies/alethiometer/__version__.py
 '''
 
-VERSION = (1, 0, 7)
+VERSION = (1, 0, 8)
 
 __version__ = '.'.join(map(str, VERSION))
