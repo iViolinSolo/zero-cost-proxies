af15c4b1b1765526634825bd0c0875532121a20d
MH.Xu <Minghao.Xu.1@warwick.ac.uk>
2023-04-28 22:25:51 +0100

    add

--- tests/test_zc.py
+++ tests/test_zc.py
@@ -1,7 +1,7 @@
 '''
 Author: ViolinSolo
 Date: 2023-04-07 19:25:08
-LastEditTime: 2023-04-28 21:36:45
+LastEditTime: 2023-04-28 22:20:35
 LastEditors: ViolinSolo
 Description: Test zc-proxies.
 FilePath: /zero-cost-proxies/tests/test_zc.py
@@ -44,9 +44,39 @@ def test_zc_proxies():
     train_loader, test_loader = get_cifar_dataloaders(64, 64, 'cifar10', 2)
 
     from alethiometer import calc_zc_metrics
-    mts = ['snip', 'grasp', 'grad_norm', 'synflow', 'nwot', 'lnwot', 'nwot_relu', 'zen', 'tenas', 'lrn', 'ntk']
+    mts = ['snip', 'grasp', 'grad_norm', 'synflow', 'nwot', 'lnwot', 'nwot_relu', 'tenas', 'lrn', 'ntk']
     results = calc_zc_metrics(metrics=mts, model=net, train_queue=train_loader, device=device, aggregate=True)
     print(results)
+    
+
+    # ==================== test zen ====================
+    # test zen
+    print('-'*20)
+    print('test zen .....')
+    import types
+    def forward_pre_GAP(self, x: torch.Tensor) -> list:
+        outputs = []
+
+        def hook_fn(module, inputs, output_t):
+            # print(f'Input tensor shape: {inputs[0].shape}')
+            # print(f'Output tensor shape: {output_t.shape}')
+            outputs.append(inputs[0])
+
+        for m in self.modules():
+            if isinstance(m, torch.nn.AdaptiveAvgPool2d):
+                m.register_forward_hook(hook_fn)
+
+        self.forward(x, None)
+
+        assert len(outputs) == 1
+        return outputs[0]
+    
+    if not hasattr(net, 'forward_before_global_avg_pool'):
+        net.forward_before_global_avg_pool = types.MethodType(forward_pre_GAP, net)
+
+    results = calc_zc_metrics(metrics=['zen'], model=net, train_queue=train_loader, device=device, aggregate=True)
+    print(results)
+
 
 
 if __name__ == "__main__":
