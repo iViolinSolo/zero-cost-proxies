dcf193242a7e0e2bda3413ea4740c92d2d5b6d59
Anonymous User <a@b.com>
2023-05-05 17:16:58 +0100

    update to new verion, v1.1.1

--- alethiometer/__version__.py
+++ alethiometer/__version__.py
@@ -1,12 +1,12 @@
 '''
 Author: ViolinSolo
 Date: 2023-04-07 16:08:57
-LastEditTime: 2023-04-30 17:24:35
+LastEditTime: 2023-05-05 17:15:55
 LastEditors: ViolinSolo
 Description: 
 FilePath: /zero-cost-proxies/alethiometer/__version__.py
 '''
 
-VERSION = (1, 1, 0)
+VERSION = (1, 1, 1)
 
 __version__ = '.'.join(map(str, VERSION))
